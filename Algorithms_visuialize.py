2258a1c7c5f6e327965e72e9e3053ba41bbf8cff
U-K-06 <utkarsh7khajuria8@gmail.com>
2025-05-23 06:40:50 +0530

    VISULIZATION WORKS

--- Algorithms_visuialize.py
+++ Algorithms_visuialize.py
@@ -3,15 +3,9 @@ from time import sleep
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 class Algorithms:
-    def __init__(self, array: list[int], iterations: int, shuffling: bool = True):
+    def __init__(self, array: list[int]):
         self.array = array
         self.copy = array.copy()
-        self.iterations = iterations
-        self.shuffling = shuffling
-        self.fig, self.ax = plt.subplots()
-        self.bars = self.ax.bar(range(len(self.array)),[i*2 for i in self.array],width=0.7,color="DarkGreen")
-        self.ax.set_ylim(0, max(self.array) * 1.3)
-        self.ax.set_xticks(range(len(self.array)),list(map(str,self.array)))
 
     def shuffle_array(self):
         if self.shuffling:
@@ -33,11 +27,8 @@ class Algorithms:
                 break
         yield self.array.copy()  # Yield final state
         self.reset_array()
-    @staticmethod
-    def update(frame,ax,bars,data):
-        for bar,height in zip(bars,data[frame]):
-            bar.set_height(height)
-        ax.set_xticklabels([str(val) for val in data[frame]])
+
+
 
     def selection_sort(self):
         for i in range(0,len(self.array)):
@@ -55,20 +46,6 @@ class Algorithms:
     
     def quick_sort(self):
         pass
-    def run(self,func):
-        snaps = list(func())
-        self.animation = FuncAnimation(
-            self.fig,
-            self.update,
-            fargs=(self.ax, self.bars,snaps),
-            frames=len(self.array)*self.iterations,  
-            interval=300, 
-            repeat=False,
-        )
-        plt.show()
-        
-
-
-a = Algorithms([225, 42, 345, 103, 490, 273, 100, 469, 188, 7, 150, 420, 98, 260, 119, 363, 198, 287, 8, 364],5,True)
-a.run(a.bubble_sort)
+    def get_snaps(self,algo):
+        return list(algo())
 
---
+++ frame_generator.py
@@ -0,0 +1,56 @@
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+class FrameGenerator:
+    def __init__(self,data,frames):
+        self.data = data
+        self.frames = frames #snap of each state of array
+        self.fig,self.ax = plt.subplots()
+        self.bars = self.ax.bar(range(len(self.data)),[i*2 for i in self.data])
+        self.ax.set_ylim(0,max(self.data)*1.3)
+        self.ax.set_xticks(range(len(data)),[*map(str,self.data)])
+        self.current_frame = 0
+        self.is_playing = False
+        self.fig.canvas.mpl_connect('key_press_event', self.update)
+        self.animation = FuncAnimation(self.fig,self.play,frames=self.frame_generator(),interval=100,repeat=False)
+
+    def frame_generator(self):
+        while True:
+            yield self.current_frame
+            if self.is_playing:
+                self.current_frame  = (self.current_frame+1)%len(self.frames)
+
+    def update(self,event):
+            if event.key == "right":
+                self.current_frame  = min(self.current_frame + 1, len(self.frames) - 1)
+                print("Animation Paused\nShowing Next State")
+            if event.key == "left":
+                self.current_frame  = max(self.current_frame - 1, 0)
+                print("Animation Paused\nShowing Previous State")
+            if event.key == "p":
+                self.is_playing = False
+                print("Animation Paused")
+            if event.key == "enter":
+                self.is_playing = True
+                print("ENTER PRESSED RUNNING ANIMation")
+
+            if event.key == "escape":
+                self.animation.save
+                plt.close(self.fig)
+                self.fig.canvas.draw_idle()
+
+
+    def play(self,frame):
+        if 0 <= frame < len(self.frames):
+                for bar, height in zip(self.bars, self.frames[frame]):
+                    bar.set_height(height)
+                self.ax.set_xticklabels([str(val) for val in self.frames[frame]])
+    
+from Algorithms_visuialize import Algorithms
+data = [225, 42, 345, 103, 490, 273, 100, 469, 188, 7, 150, 420, 98, 260, 119, 363, 198, 287, 8, 364]
+a = Algorithms(data)
+snaps = a.get_snaps(a.bubble_sort)
+
+b = FrameGenerator(data,snaps)
+
+plt.show()
